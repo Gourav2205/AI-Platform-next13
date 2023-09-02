3d57c3d69eadd907c73af0c87873de6a73e30fae
Gourav2205 <gouravbiswas897@gmail.com>
2023-09-03 00:32:22 +0530

    Third

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image'
 import { Button } from "@/components/ui/button"
 
 
-export default function Home() {
+const Dashboard = () => {
     return (
         <main>
             <Button variant="default" className='mx-3 my-3'>Click me</Button>
         </main>
     )
 }
+
+export default Dashboard;
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { UserButton } from "@clerk/nextjs";
 
 const page = () => {
     return (
         <div>
-            Landing page (Unprotected)
+            Landing page (protected)
+            <div>
+                <Link href="/sign-in">
+                    <Button>
+                        Login
+                    </Button>
+                </Link>
+                <Link href="/sign-in">
+                    <Button>
+                        Register
+                    </Button>
+                </Link>
+                <Link href="/dashboard">
+                    <Button>
+                        Dashboard
+                    </Button>
+                </Link>
+            </div>
+            <UserButton />
         </div>
     )
 }
